3b5c9c4e96eec9ebfafdd3cd359007c98f4c48dc
vollh4rd <59693437+vollh4rD@users.noreply.github.com>
2022-08-26 22:47:34 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,25 @@
+
+
+import click
+import io 
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+
+@click.command()
+@click.argument('filename')
+
+def stop_word(filename):
+    stop_words = set(stopwords.words('english')) 
+    with click.open_file(filename) as file1:
+        """ file1 = click.open('filename') """ 
+        line = file1.read()
+        """ words = line.split()  """
+        for r in line: 
+            if not r in stop_words: 
+                appendFile = open('filteredtext.txt','a')
+                
+                appendFile.write(r) 
+                appendFile.close() 
+
+if __name__ == '__main__':
+    stop_word()
